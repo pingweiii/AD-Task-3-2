e423e8f6b529a2bcea5c61965f05add2f1b6f1b8
pingweiii <pwchen31@gmail.com>
2025-06-22 23:33:03 +0800

    refactor (envSetter.util.php): added the pgconfig

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,5 +1,21 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
+
+// Manually override PG_HOST for local execution
+if (php_sapi_name() === 'cli') {
+    $_ENV['PG_HOST'] = 'localhost';
+}
+
+$pgConfig = [
+    'host' => $_ENV['PG_HOST'],
+    'port' => $_ENV['PG_PORT'],
+    'user' => $_ENV['PG_USER'],
+    'pass' => $_ENV['PG_PASS'],
+    'dbname' => $_ENV['PG_DB']
+];
+
+?>
