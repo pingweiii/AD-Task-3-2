d9acf7d8005fdf48330240e28f785c258b25c0d8
pingweiii <pwchen31@gmail.com>
2025-07-01 20:44:07 +0800

    feat (user.model.sql): added the username, password, firstname, and lastname

--- database/user.model.sql
+++ database/user.model.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
