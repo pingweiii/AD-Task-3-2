8c3d0d10d7eacd30d0a3d384232443a24f407b05
pingweiii <pwchen31@gmail.com>
2025-07-01 21:42:25 +0800

    feat (dbSeederPostgresql.util.php): added the codes given in the workbook

--- utils/dbSeederPostgresql.util.php
+++ utils/dbSeederPostgresql.util.php
@@ -15,7 +15,7 @@ $pdo = new PDO($dsn, $pgConfig['user'], $pgConfig['pass'], [
 ]);
 
 $schemaFiles = [
-  'users'          => 'database/user.model.sql',
+  'users'          => 'database/users.model.sql',
   'meeting'       => 'database/meeting.model.sql',
   'meeting_users'  => 'database/meeting_users.model.sql',
 ];
@@ -36,4 +36,38 @@ foreach (array_keys($schemaFiles) as $table) {
   $pdo->exec("TRUNCATE TABLE {$table} RESTART IDENTITY CASCADE;");
 }
 echo "Reset complete!\n";
+
+echo "Seeding users…\n";
+
+// query preparations. NOTE: make sure they matches order and number
+$stmt = $pdo->prepare("
+    INSERT INTO users (username, role, first_name, last_name, password)
+    VALUES (:username, :role, :fn, :ln, :pw)
+");
+
+// plug-in datas from the staticData and add to the database
+foreach ($users as $u) {
+  $stmt->execute([
+    ':username' => $u['username'],
+    ':role' => $u['role'],
+    ':fn' => $u['first_name'],
+    ':ln' => $u['last_name'],
+    ':pw' => password_hash($u['password'], PASSWORD_DEFAULT),
+  ]);
+}
+
+$stmt = $pdo->query("SELECT * FROM users");
+
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($users as $user) {
+    echo "---------------------------\n";
+    echo "User ID: " . $user['id'] . "\n";
+    echo "Username: " . $user['username'] . "\n";
+    echo "First Name: " . $user['first_name'] . "\n";
+    echo "Last Name: " . $user['last_name'] . "\n";
+    echo "Role: " . $user['role'] . "\n";
+    echo "---------------------------\n";
+}
+
 ?>
