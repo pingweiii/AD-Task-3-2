e53ef568d808d7a79ecccbcbe9a16d43eb75200e
pingweiii <pwchen31@gmail.com>
2025-07-01 21:38:25 +0800

    feat (users.staticData.php): added the array for username, firstname, lastname, password

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,5 @@
+<?php
+// the table of users are compose of following columns: id, username, first_name, last_name, password, role
+return [
+    ['username' => 'john.smith', 'first_name' => 'John', 'last_name' => 'Smith', 'password' => 'p@ssW0rd1234', 'role' => 'designer'],
+];
