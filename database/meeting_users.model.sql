1265f2c291fe8c5ca657def51f886d97b64d487b
pingweiii <pwchen31@gmail.com>
2025-07-01 20:41:22 +0800

    refactor (meeting_users.model.sql): renamed the id to uuid

--- database/meeting_users.model.sql
+++ database/meeting_users.model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS meeting_users (
-    meeting_id INTEGER NOT NULL REFERENCES meeting (id),
-    user_id INTEGER NOT NULL REFERENCES users (id),
+    meeting_id uuid NOT NULL REFERENCES meeting (id),
+    user_id uuid NOT NULL REFERENCES users (id),
     role VARCHAR(50) NOT NULL,
     PRIMARY KEY (meeting_id, user_id)
 );
